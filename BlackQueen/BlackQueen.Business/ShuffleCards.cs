b1798308241bbb0085f9c77d606bf99a7aae9c70
Ravindra.Gaikwad1 <ravindra.gaikwad@sphanalytics.com>
2023-01-09 23:09:18 -0500

    01092023

---
+++ BlackQueen/BlackQueen.Business/ShuffleCards.cs
@@ -0,0 +1,29 @@
+﻿using BlackQueen.Entities;
+
+namespace BlackQueen.Business;
+
+public static class ShuffleCards
+{
+    public static List<Card> Shuffle()
+    {
+        List<Card> cards = new List<Card>();
+
+        cards.Add(new Card(1, "Ace"));
+        cards.Add(new Card(2, "2"));
+        cards.Add(new Card(3, "3"));
+        cards.Add(new Card(4, "4"));
+        cards.Add(new Card(5, "5"));
+        cards.Add(new Card(6, "6"));
+        cards.Add(new Card(7, "7"));
+        cards.Add(new Card(8, "8"));
+        cards.Add(new Card(9, "9"));
+        cards.Add(new Card(10, "10"));
+        cards.Add(new Card(11, "Jack"));
+        cards.Add(new Card(12, "Queen"));
+        cards.Add(new Card(13, "King"));
+
+        Random rand = new Random();
+        var shuffled = cards.OrderBy(_ => rand.Next()).ToList();
+        return shuffled;
+    }
+}
---
+++ BlackQueen/BlackQueen.Entities/Card.cs
@@ -0,0 +1,13 @@
+﻿namespace BlackQueen.Entities;
+public class Card
+{
+    public Card(int _cardID, string _cardName)
+    {
+        this.cardId = _cardID;
+        this.cardName = _cardName;
+    }
+
+    public int cardId { get; set; }
+    public string? cardName { get; set; }
+}
+
